91306f991867251ca49b3e627cc3f9ccb230cdc6
rogengell <niel4921@easv365.dk>
2024-09-30 14:51:35 +0200

    nginx works for multible containers

--- LoggingApi/Request/loggingRequest.cs
+++ LoggingApi/Request/loggingRequest.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 public class loggingRequest
 {
+    [Required]
     public int TraceId { get; set; }
+    [Required]
     public int SpanId { get; set; }
     public int? ParentSpanId { get; set; }
     public string? LoggingString { get; set; }
+    [Required]
     public DateTime Time { get; set; }
 }
