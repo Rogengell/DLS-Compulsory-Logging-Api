f98c41fcaafb827053ea9499be828f9fd65e2abe
rogengell <niel4921@easv365.dk>
2024-10-01 16:40:13 +0200

    some migration changes, and inprovments

--- LoggingApi/Migrations.Dockerfile
+++ LoggingApi/Migrations.Dockerfile
@@ -1,5 +1,7 @@
 ﻿# Esben and Asbjørn fandt ud af dette
 FROM mcr.microsoft.com/dotnet/sdk:8.0
+RUN apt-get update && apt-get install -y netcat-openbsd
+
 WORKDIR /app
 
 COPY . .
--- run-ef-database-update.sh
+++ run-ef-database-update.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # Esben and Asbjørn fandt ud af dette
+
+# for the DB to be fully startet and ready for changes if any
+sleep 60
+
+while ! nc -z logging-db 1433; do
+  sleep 5
+done
+
 cd /app/LoggingApi
 dotnet tool install --global dotnet-ef
 /root/.dotnet/tools/dotnet-ef database update
